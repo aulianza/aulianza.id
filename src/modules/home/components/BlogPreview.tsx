219d5c2cebc16fa237d90f2256a66862778c3ef2
Ryan Aulia <hello@aulianza.id>
2023-05-27 22:59:40 +0700

    feat: add view all blog link

--- src/modules/home/components/BlogPreview.tsx
+++ src/modules/home/components/BlogPreview.tsx
@@ -2,16 +2,19 @@ import React, { FC } from "react";
 import Icon from "supercons";
 
 import BlogList from "@/modules/blog/components/BlogList";
+import Link from "next/link";
 
 const BlogPreview: FC = () => {
 	return (
 		<div className="space-y-6">
 			<div className="flex items-center justify-between">
 				<h2 className="text-xl font-semibold">Latest Articles</h2>
-				<div className="flex gap-1 hover:gap-3 transition-all duration-300 cursor-pointer text-sm text-neutral-700 dark:text-neutral-400">
-					<span>View All Articles</span>
-					<Icon glyph="enter" size={22} />
-				</div>
+				<Link href="/blog">
+					<div className="flex gap-1 hover:gap-3 transition-all duration-300 cursor-pointer text-sm text-neutral-700 dark:text-neutral-400">
+						<span>View All Articles</span>
+						<Icon glyph="enter" size={22} />
+					</div>
+				</Link>
 			</div>
 			<BlogList />
 		</div>

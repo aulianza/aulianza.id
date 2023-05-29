1233cf9a1f5028f07cb306896fac6a7e066c3ef9
Ryan Aulia <hello@aulianza.id>
2023-05-29 11:30:09 +0700

    fix: overview item

--- src/modules/dashboard/components/Contributions/OverviewItem.tsx
+++ src/modules/dashboard/components/Contributions/OverviewItem.tsx
@@ -8,13 +8,15 @@ interface OverviewItemProps {
 }
 
 const OverviewItem: FC<OverviewItemProps> = ({ label, value, unit = "" }) => (
-	<div className="flex flex-col rounded-xl bg-neutral-100 py-2 px-4 dark:bg-neutral-800">
+	<div className="flex flex-col self-center rounded-xl bg-neutral-100 py-3 px-4 dark:bg-neutral-800">
 		<span className="text-sm dark:text-neutral-400">{label}</span>
-		<AnimateCounter
-			className="text-xl lg:text-2xl font-medium text-green-600"
-			total={value}
-		/>
-		{unit && <span className="text-sm dark:text-neutral-400">{unit}</span>}
+		<div>
+			<AnimateCounter
+				className="text-xl lg:text-2xl font-medium text-green-600"
+				total={value}
+			/>
+			{unit && <span className="text-sm dark:text-neutral-400"> {unit}</span>}
+		</div>
 	</div>
 );
 

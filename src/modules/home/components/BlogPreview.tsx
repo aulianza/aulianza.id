63dfa69ea8b372bc73c17d239a12f7635c88591b
Ryan Aulia <hello@aulianza.id>
2023-05-30 13:46:36 +0700

    fix: view all button

--- src/modules/home/components/BlogPreview.tsx
+++ src/modules/home/components/BlogPreview.tsx
@@ -7,25 +7,27 @@ import SectionSubHeading from "@/common/components/elements/SectionSubHeading";
 import BlogList from "@/modules/blog/components/BlogList";
 
 const BlogPreview: FC = () => {
-	return (
-		<section className="space-y-6">
-			<div className="flex items-center justify-between">
-				<SectionHeading
-					title="Latest Articles"
-					icon={<Icon glyph="rss" size={32} />}
-				/>
-				<SectionSubHeading>
-					<Link href="/blog">
-						<div className="flex gap-1 hover:gap-3 transition-all duration-300 cursor-pointer text-sm text-neutral-700 dark:text-neutral-400 hover:text-neutral-700 hover:dark:text-neutral-300">
-							<span>View All Articles</span>
-							<Icon glyph="enter" size={22} />
-						</div>
-					</Link>
-				</SectionSubHeading>
-			</div>
-			<BlogList />
-		</section>
-	);
+  return (
+    <section className="space-y-6">
+      <div className="flex items-center justify-between">
+        <SectionHeading
+          title="Latest Articles"
+          icon={<Icon glyph="rss" size={32} />}
+        />
+        <SectionSubHeading>
+          <Link href="/blog">
+            <div className="flex gap-1 hover:gap-3 transition-all duration-300 cursor-pointer text-sm text-neutral-700 dark:text-neutral-400 hover:text-neutral-700 hover:dark:text-neutral-300 mt-1">
+              <div className="flex">
+                View All <span className="hidden sm:block ml-1">Articles</span>
+              </div>
+              <Icon glyph="enter" size={22} />
+            </div>
+          </Link>
+        </SectionSubHeading>
+      </div>
+      <BlogList />
+    </section>
+  );
 };
 
 export default BlogPreview;

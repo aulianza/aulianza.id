133dfd79a2d8848888b1ac8efc283e19d8fe7bfc
Ryan Aulia <hello@aulianza.id>
2023-05-30 19:16:04 +0700

    fix: update menu

--- src/common/constant/menu.tsx
+++ src/common/constant/menu.tsx
@@ -4,73 +4,73 @@ import { MenuItemProps } from "../types/menu";
 const iconSize = 26;
 
 export const MENU_ITEMS: MenuItemProps[] = [
-	{
-		name: "Home",
-		href: "/",
-		icon: <Icon glyph="home" size={iconSize} />,
-		is_show: true,
-	},
-	{
-		name: "Dashboard",
-		href: "/dashboard",
-		icon: <Icon glyph="grid" size={iconSize} />,
-		is_show: true,
-	},
-	{
-		name: "Projects",
-		href: "/projects",
-		icon: <Icon glyph="explore" size={iconSize} />,
-		is_show: true,
-	},
-	{
-		name: "Blog",
-		href: "/blog",
-		icon: <Icon glyph="post" size={iconSize} />,
-		is_show: true,
-	},
-	{
-		name: "Tools",
-		href: "/tools",
-		icon: <Icon glyph="bug" size={iconSize} />,
-		is_show: false,
-	},
-	{
-		name: "About",
-		href: "/about",
-		icon: <Icon glyph="profile" size={iconSize} />,
-		is_show: true,
-	},
-	{
-		name: "Contact",
-		href: "/contact",
-		icon: <Icon glyph="email" size={iconSize} />,
-		is_show: true,
-	},
+  {
+    name: "Home",
+    href: "/",
+    icon: <Icon glyph="home" size={iconSize} />,
+    is_show: true,
+  },
+  {
+    name: "Dashboard",
+    href: "/dashboard",
+    icon: <Icon glyph="grid" size={iconSize} />,
+    is_show: true,
+  },
+  {
+    name: "Projects",
+    href: "/projects",
+    icon: <Icon glyph="explore" size={iconSize} />,
+    is_show: true,
+  },
+  {
+    name: "Blog",
+    href: "/blog",
+    icon: <Icon glyph="post" size={iconSize} />,
+    is_show: true,
+  },
+  {
+    name: "Tools",
+    href: "/tools",
+    icon: <Icon glyph="bug" size={iconSize} />,
+    is_show: false,
+  },
+  {
+    name: "About",
+    href: "/about",
+    icon: <Icon glyph="profile" size={iconSize} />,
+    is_show: true,
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+    icon: <Icon glyph="email" size={iconSize} />,
+    is_show: true,
+  },
 ];
 
 export const SOCIAL_MEDIA: MenuItemProps[] = [
-	{
-		name: "Linkedin",
-		href: "https://www.linkedin.com/in/aulianza/",
-		icon: <Icon glyph="channels" size={iconSize} />,
-		is_show: true,
-	},
-	{
-		name: "Github",
-		href: "https://github.com/aulianza",
-		icon: <Icon glyph="github" size={iconSize} />,
-		is_show: true,
-	},
-	{
-		name: "Instagram",
-		href: "https://instagram.com/aulianza",
-		icon: <Icon glyph="instagram" size={iconSize} />,
-		is_show: true,
-	},
-	{
-		name: "Twitter",
-		href: "https://twitter.com/aulianzaa",
-		icon: <Icon glyph="twitter" size={iconSize} />,
-		is_show: true,
-	},
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/aulianza/",
+    icon: <Icon glyph="channels" size={iconSize} />,
+    is_show: true,
+  },
+  {
+    name: "Github",
+    href: "https://github.com/aulianza",
+    icon: <Icon glyph="github" size={iconSize} />,
+    is_show: true,
+  },
+  {
+    name: "Instagram",
+    href: "https://instagram.com/aulianza",
+    icon: <Icon glyph="instagram" size={iconSize} />,
+    is_show: true,
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/aulianzaa",
+    icon: <Icon glyph="twitter" size={iconSize} />,
+    is_show: false,
+  },
 ];
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -8,18 +8,19 @@ import Container from "@/common/components/elements/Container";
 import PageHeading from "@/common/components/elements/PageHeading";
 
 const PAGE_TITLE = "Contact";
-const PAGE_DESCRIPTION = "Let's get in touch!";
+const PAGE_DESCRIPTION =
+  "Feel free to get in touch and let's have a discussion about how we can work together.";
 
 const ContactPage: NextPage = () => {
-	return (
-		<>
-			<NextSeo title={`${PAGE_TITLE} - Ryan Aulia`} />
-			<Container data-aos="fade-up">
-				<PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} />
-				<Contact />
-			</Container>
-		</>
-	);
+  return (
+    <>
+      <NextSeo title={`${PAGE_TITLE} - Ryan Aulia`} />
+      <Container data-aos="fade-up">
+        <PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} />
+        <Contact />
+      </Container>
+    </>
+  );
 };
 
 export default ContactPage;

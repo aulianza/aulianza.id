ab22fddb6b286975239b859ec3ee1b5152fc48bb
Ryan Aulia <hello@aulianza.id>
2023-05-31 16:15:30 +0700

    feat: add favicon

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["aulianza.s3.ap-southeast-1.amazonaws.com", "aulianza.id"],
+    domains: [
+      "aulianza.s3.ap-southeast-1.amazonaws.com",
+      "aulianza.id",
+      "aulianza.com",
+    ],
   },
 };
 

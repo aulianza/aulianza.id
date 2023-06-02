16df697d603ecbacc67f390618e942823aff2a55
Ryan Aulia <hello@aulianza.id>
2023-06-02 13:00:40 +0700

    fix: back button

--- src/common/components/elements/BackButton.tsx
+++ src/common/components/elements/BackButton.tsx
@@ -8,12 +8,14 @@ type BackButtonProps = {
 
 const BackButton: FC<BackButtonProps> = ({ url }) => {
   return (
-    <Link href={url} passHref>
-      <div className="flex gap-1 w-max hover:gap-2 items-center pb-5 transition-all duration-300 font-medium text-neutral-600 dark:text-neutral-400">
-        <Icon glyph="back" size={32} />
-        <span>Back</span>
-      </div>
-    </Link>
+    <div className="w-fit">
+      <Link href={url} passHref>
+        <div className="flex gap-1 w-max hover:gap-2 items-center pb-5 transition-all duration-300 font-medium text-neutral-600 dark:text-neutral-400">
+          <Icon glyph="back" size={32} />
+          <span>Back</span>
+        </div>
+      </Link>
+    </div>
   );
 };
 

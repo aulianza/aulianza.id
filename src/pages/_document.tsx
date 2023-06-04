3db1d3477b50ed9af00722efd5e78f07d704f700
Ryan Aulia <hello@aulianza.id>
2023-06-04 13:44:45 +0700

    feat: add umami analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <script
+          async
+          defer
+          src='https://umami.aulianza.id/script.js'
+          data-website-id='5a78190a-bdad-48a4-901a-c7400be41ca6'
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />

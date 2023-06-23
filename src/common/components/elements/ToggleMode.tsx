cb263ae5786eca00b7e4c7e7c39e1a218cbc83ea
Ryan Aulia <hello@aulianza.id>
2023-06-23 19:54:23 +0700

    fix: toggle

--- src/common/components/elements/ToggleMode.tsx
+++ src/common/components/elements/ToggleMode.tsx
@@ -13,18 +13,13 @@ const ToggleMode: FC = () => {
   const toggleTheme = () =>
     setTheme(resolvedTheme === 'light' ? 'dark' : 'light');
 
-  const switchText = 'Switch to ';
-
   return (
     <button
       onClick={toggleTheme}
       className='flex items-center gap-2 py-2 px-3 w-full lg:hover:dark:bg-zinc-800 lg:hover:bg-gray-200 lg:hover:rounded-lg lg:hover:scale-105 lg:transition-all lg:duration-300 text-neutral-700 dark:text-neutral-400 hover:text-neutral-900 hover:dark:text-neutral-300'
     >
       <Icon glyph={isDarkTheme ? 'idea' : 'moon-fill'} size={22} />
-      <div className='flex lg:text-[15px]'>
-        <span className='hidden xl:block xl:mr-1'>{switchText}</span>
-        {isDarkTheme ? 'Light Mode' : 'Dark Mode'}
-      </div>
+      <div className='flex'>{isDarkTheme ? 'Light Mode' : 'Dark Mode'}</div>
     </button>
   );
 };

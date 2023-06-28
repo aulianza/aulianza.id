cae325e8bdae0cae412aa6c898c7c101b2aeccd8
Ryan Aulia <hello@aulianza.id>
2023-06-28 12:05:09 +0700

    fix: project detail card

--- src/modules/projects/components/ProjectDetail.tsx
+++ src/modules/projects/components/ProjectDetail.tsx
@@ -16,9 +16,11 @@ const ProjectDetail = ({
 
   return (
     <div className='space-y-8'>
-      <div className='flex flex-col items-center sm:flex-row gap-5 justify-between'>
+      <div className='flex flex-col lg:flex-row items-start lg:items-center sm:flex-row gap-5 justify-between'>
         <div className='flex items-center flex-wrap gap-2'>
-          <span className='text-[15px]'>Tech Stack :</span>
+          <span className='text-[15px] mb-1 text-neutral-700 dark:text-neutral-300'>
+            Tech Stack :
+          </span>
           <div className='flex flex-wrap items-center gap-3'>
             {stacksArray?.map((stack: string, index: number) => (
               <div key={index}>

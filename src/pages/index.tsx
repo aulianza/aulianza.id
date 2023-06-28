dc282c1d48e4ef1b7f864599bcc713e02088a71d
Ryan Aulia <hello@aulianza.id>
2023-06-28 12:31:31 +0700

    fix: update homepage from ssg to ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
-import { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
 
 import Container from '@/common/components/elements/Container';
 import Home from '@/modules/home';
 
-const HomePage: NextPage = () => {
+interface HomePageProps {
+  title: string;
+}
+
+const HomePage = ({ title }: HomePageProps) => {
   return (
     <>
-      <NextSeo title='Ryan Aulia - Personal Website' />
+      <NextSeo title={title} />
       <Container data-aos='fade-up'>
         <Home />
       </Container>
     </>
   );
+};
+
+HomePage.getInitialProps = async () => {
+  const title = 'Ryan Aulia - Personal Website';
+  return { title };
 };
 
 export default HomePage;

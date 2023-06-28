222bf689dc509ed48deefe6470299029ed3dd683
Ryan Aulia <hello@aulianza.id>
2023-06-28 09:18:16 +0700

    fix: adjust mobile menu animation

--- src/common/components/sidebar/MobileMenu.tsx
+++ src/common/components/sidebar/MobileMenu.tsx
@@ -8,9 +8,8 @@ const MobileMenu: FC = () => {
   return (
     <motion.div
       className='my-5'
-      initial={{ opacity: 0, y: -20 }}
+      initial={{ y: -100 }}
       animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: -20 }}
       transition={{ duration: 0.3 }}
     >
       <Breakline className='mt-2' />

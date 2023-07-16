ab2fc44a05f1f4d060082a85e415481db39cc4e6
Ryan Aulia <hello@aulianza.id>
2023-07-16 09:58:53 +0700

    fix: update giscuss comment

--- src/pages/learn/[content]/[slug].tsx
+++ src/pages/learn/[content]/[slug].tsx
@@ -2,9 +2,11 @@ import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import { NextSeo } from 'next-seo';
 
 import BackButton from '@/common/components/elements/BackButton';
+import Breakline from '@/common/components/elements/Breakline';
 import Container from '@/common/components/elements/Container';
 import loadMdxFiles from '@/common/libs/mdx';
 import { MdxFileContentProps } from '@/common/types/learn';
+import GiscusComment from '@/modules/blog/components/GiscusComment';
 import ContentDetail from '@/modules/learn/components/ContentDetail';
 import ContentDetailHeader from '@/modules/learn/components/ContentDetailHeader';
 
@@ -14,17 +16,44 @@ interface ContentDetailPageProps {
 
 const LearnContentDetailPage: NextPage<ContentDetailPageProps> = ({ data }) => {
   const { content, frontMatter } = data;
+
   const meta = frontMatter;
 
   const PAGE_TITLE = meta?.title;
+  const PAGE_DESCRIPTION = `Learn ${meta?.category} - ${PAGE_TITLE} with detailed explanations`;
 
   return (
     <>
-      <NextSeo title={`Learn ${meta?.category} : ${PAGE_TITLE} - Ryan Aulia`} />
+      <NextSeo
+        title={`Learn ${meta?.category} : ${PAGE_TITLE} - Ryan Aulia`}
+        description={PAGE_DESCRIPTION}
+        openGraph={{
+          type: 'article',
+          article: {
+            publishedTime: meta?.updated_at,
+            modifiedTime: meta?.updated_at,
+            authors: ['Ryan Aulia'],
+          },
+          images: [
+            {
+              url: meta?.cover_url as string,
+            },
+          ],
+          siteName: 'Ryan Aulia',
+        }}
+      />
       <Container data-aos='fade-up'>
         <BackButton />
         <ContentDetailHeader {...meta} />
-        {content && <ContentDetail content={content} />}
+        {content && (
+          <>
+            <ContentDetail content={content} />
+            <Breakline className='mt-14 mb-14' />
+            <section id='comments'>
+              <GiscusComment />
+            </section>
+          </>
+        )}
       </Container>
     </>
   );

df7e6003410cfdb8911f86fd7589ea46b56450e6
Ryan Aulia <hello@aulianza.id>
2023-07-20 10:06:43 +0700

    feat: add table content style

--- src/common/components/elements/MDXComponent.tsx
+++ src/common/components/elements/MDXComponent.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
@@ -6,6 +7,16 @@ import CodeBlock from './CodeBlock';
 interface MarkdownRendererProps {
   children: string;
 }
+
+interface TableProps {
+  children: ReactNode;
+}
+
+const Table = ({ children }: TableProps) => (
+  <div className='table-container'>
+    <table className='table w-full'>{children}</table>
+  </div>
+);
 
 const MDXComponent = ({ children }: MarkdownRendererProps) => {
   return (
@@ -43,6 +54,17 @@ const MDXComponent = ({ children }: MarkdownRendererProps) => {
             className='pl-6 py-3 text-lg border-l-[5px] border-neutral-700 border-l-cyan-500 font-medium bg-neutral-200 dark:bg-neutral-800 rounded-br-2xl text-cyan-800 dark:text-cyan-200'
             {...props}
           />
+        ),
+        table: (props) => <Table {...props} />,
+        th: (props) => (
+          <th className='border dark:border-neutral-600 py-1 px-3 text-left'>
+            {props.children}
+          </th>
+        ),
+        td: (props) => (
+          <td className='border dark:border-neutral-600  py-1 px-3'>
+            {props.children}
+          </td>
         ),
       }}
     >

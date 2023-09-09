340fb76bfea03ee2fc2b76fdc7df7d39c35ec616
Ryan Aulia <hello@aulianza.id>
2023-09-09 15:55:34 +0700

    feat: add watermark copyright

--- src/modules/playground/components/Playground.tsx
+++ src/modules/playground/components/Playground.tsx
@@ -60,43 +60,52 @@ const Playground = ({ isHeading = false }: PlaygroundProps) => {
           </p>
         </div>
       )}
-      <div className='flex flex-col md:flex-row bg-neutral-900'>
-        <div className='w-full md:w-1/2 border border-neutral-700 rounded-l-sm'>
-          <PlaygroundHeader title='JavaScript'>
-            <div className='flex items-center gap-5'>
-              <Tooltip title='Clear Editor'>
-                <div className='cursor-pointer' onClick={() => setCode('')}>
-                  <ClearIcon size={18} className='text-red-400' />
-                </div>
-              </Tooltip>
-              <Tooltip title='Run Code'>
-                <div className='cursor-pointer' onClick={runCode}>
-                  <PlayIcon size={18} className='text-sky-500' />
-                </div>
-              </Tooltip>
+      <div>
+        <div className='flex flex-col md:flex-row bg-neutral-900'>
+          <div className='w-full md:w-1/2 border border-neutral-700 rounded-l-sm'>
+            <PlaygroundHeader title='JavaScript'>
+              <div className='flex items-center gap-5'>
+                <Tooltip title='Clear Editor'>
+                  <div className='cursor-pointer' onClick={() => setCode('')}>
+                    <ClearIcon size={18} className='text-red-400' />
+                  </div>
+                </Tooltip>
+                <Tooltip title='Run Code'>
+                  <div className='cursor-pointer' onClick={runCode}>
+                    <PlayIcon size={18} className='text-sky-500' />
+                  </div>
+                </Tooltip>
+              </div>
+            </PlaygroundHeader>
+            <div>
+              <CodeEditor
+                code={code}
+                height='500px'
+                onChange={(newCode) =>
+                  newCode !== undefined && setCode(newCode)
+                }
+              />
             </div>
-          </PlaygroundHeader>
-          <div>
-            <CodeEditor
-              code={code}
-              height='500px'
-              onChange={(newCode) => newCode !== undefined && setCode(newCode)}
-            />
+          </div>
+          <div className='w-full md:w-1/2 border border-neutral-700 border-t-0 md:border-l-0 md:border-t rounded-r-sm'>
+            <PlaygroundHeader title='Console'>
+              <div className='flex items-center'>
+                <Tooltip title='Clear Console'>
+                  <div className='cursor-pointer' onClick={() => setOutput('')}>
+                    <ClearIcon size={18} className='text-red-400' />
+                  </div>
+                </Tooltip>
+              </div>
+            </PlaygroundHeader>
+            <div>
+              <ConsoleOutput output={output} isError={isError} height='500px' />
+            </div>
           </div>
         </div>
-        <div className='w-full md:w-1/2 border border-neutral-700 border-l-0 rounded-r-sm'>
-          <PlaygroundHeader title='Console'>
-            <div className='flex items-center'>
-              <Tooltip title='Clear Console'>
-                <div className='cursor-pointer' onClick={() => setOutput('')}>
-                  <ClearIcon size={18} className='text-red-400' />
-                </div>
-              </Tooltip>
-            </div>
-          </PlaygroundHeader>
-          <div>
-            <ConsoleOutput output={output} isError={isError} height='500px' />
-          </div>
+        <div className='flex justify-end bg-neutral-900 border border-neutral-700 border-t-0 py-1 px-2'>
+          <span className='text-sm font-sora text-neutral-500 '>
+            by aulianza
+          </span>
         </div>
       </div>
     </>
@@ -108,7 +117,7 @@ export default Playground;
 const PlaygroundHeader = ({ title, children }: PlaygroundHeaderProps) => {
   return (
     <div className='flex justify-between py-2 px-3 border border-b-neutral-700 border-t-0 border-x-0'>
-      <div className='py-1 px-2 bg-neutral-600 text-xs rounded-md text-neutral-50'>
+      <div className='py-1 px-2 bg-neutral-600 text-xs rounded-md text-neutral-50 font-sora'>
         {title}
       </div>
       {children && children}

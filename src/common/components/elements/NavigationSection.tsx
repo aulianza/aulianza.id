b43fe95a2b2a3b8d7dfb9a8f44d3e785601af8ac
Ryan Aulia <hello@aulianza.id>
2023-09-13 12:55:05 +0700

    feat: add content bottom navigation

---
+++ src/common/components/elements/NavigationSection.tsx
@@ -0,0 +1,41 @@
+import ButtonNavigation from './ButtonNavigation';
+
+interface NavigationSectionProps {
+  currentIndex: number;
+  totalItems: number;
+  handleNext: () => void;
+  handlePrevious: () => void;
+  previousTitle: string | null;
+  nextTitle: string | null;
+}
+
+const NavigationSection = ({
+  currentIndex,
+  totalItems,
+  handleNext,
+  handlePrevious,
+  previousTitle,
+  nextTitle,
+}: NavigationSectionProps) => {
+  return (
+    <div className='flex justify-between my-6 border-t border-b dark:border-neutral-700 border-gray-300 py-5'>
+      {currentIndex !== 0 && (
+        <ButtonNavigation
+          onClick={handlePrevious}
+          action='previous'
+          title={previousTitle}
+        />
+      )}
+      <div className='flex-grow'></div>
+      {currentIndex !== totalItems - 1 && (
+        <ButtonNavigation
+          onClick={handleNext}
+          action='next'
+          title={nextTitle}
+        />
+      )}
+    </div>
+  );
+};
+
+export default NavigationSection;
--- src/common/components/layouts/header/HeaderTop.tsx
+++ src/common/components/layouts/header/HeaderTop.tsx
@@ -5,7 +5,6 @@ import { MdVerified as VerifiedIcon } from 'react-icons/md';
 
 import { MENU_ITEMS } from '@/common/constant/menu';
 import { CommandPaletteContext } from '@/common/context/CommandPaletteContext';
-import useIsMobile from '@/common/hooks/useIsMobile';
 
 import Image from '../../elements/Image';
 import ThemeToggleButton from '../../elements/ThemeToggleButton';
@@ -14,7 +13,6 @@ import MenuItem from '../../sidebar/MenuItem';
 import Profile from '../../sidebar/Profile';
 
 const HeaderTop = () => {
-  const isMobile = useIsMobile();
   const { setIsOpen } = useContext(CommandPaletteContext);
 
   const menus = MENU_ITEMS?.filter((item) => item?.isShow);
@@ -22,46 +20,45 @@ const HeaderTop = () => {
 
   return (
     <header>
-      {isMobile ? (
-        <Profile />
-      ) : (
-        <div className='flex gap-5 items-center justify-between py-8 mx-8'>
-          <div className='flex items-center gap-5'>
-            <Image
-              src='/images/aulianza.png'
-              alt='Ryan Aulia'
-              width={40}
-              height={40}
-              rounded='rounded-full'
-              className='lg:hover:scale-105'
-            />
-            <div className='flex gap-2 items-center'>
-              <Link href='/' passHref>
-                <h2 className='flex-grow text-lg lg:text-xl font-sora font-medium'>
-                  Ryan Aulia
-                </h2>
-              </Link>
-              <Tooltip title='Verified'>
-                <VerifiedIcon size={18} className='text-blue-400' />
-              </Tooltip>
-            </div>
-          </div>
-
-          <div className='flex items-center gap-5'>
-            <div className='flex gap-1 items-center' data-aos='flip-up'>
-              {filterdMenu?.map((item, index: number) => (
-                <MenuItem key={index} {...item} hideIcon />
-              ))}
-            </div>
-            <CommandIcon
-              onClick={() => setIsOpen(true)}
-              className='cursor-pointer'
-              size={20}
-            />
-            <ThemeToggleButton />
+      <div className='hidden xl:flex gap-5 items-center justify-between py-8 mx-8'>
+        <div className='flex items-center gap-5'>
+          <Image
+            src='/images/aulianza.png'
+            alt='Ryan Aulia'
+            width={40}
+            height={40}
+            rounded='rounded-full'
+            className='lg:hover:scale-105'
+          />
+          <div className='flex gap-2 items-center'>
+            <Link href='/' passHref>
+              <h2 className='flex-grow text-lg lg:text-xl font-sora font-medium'>
+                Ryan Aulia
+              </h2>
+            </Link>
+            <Tooltip title='Verified'>
+              <VerifiedIcon size={18} className='text-blue-400' />
+            </Tooltip>
           </div>
         </div>
-      )}
+
+        <div className='flex items-center gap-5'>
+          <div className='flex gap-1 items-center' data-aos='flip-up'>
+            {filterdMenu?.map((item, index: number) => (
+              <MenuItem key={index} {...item} hideIcon />
+            ))}
+          </div>
+          <CommandIcon
+            onClick={() => setIsOpen(true)}
+            className='cursor-pointer'
+            size={20}
+          />
+          <ThemeToggleButton />
+        </div>
+      </div>
+      <div className='block xl:hidden'>
+        <Profile />
+      </div>
     </header>
   );
 };

aaf67ed5231a591d1e94787842ca89e6d94a08a8
aulianza <aulianza.dev@gmail.com>
2023-11-13 09:51:18 +0700

    fix: ipad mini screen ratio on blog

--- src/modules/blog/components/BlogListNew.tsx
+++ src/modules/blog/components/BlogListNew.tsx
@@ -121,7 +121,7 @@ const BlogListNew = () => {
           />
         </div>
 
-        <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-5'>
           {!isValidating ? (
             <>
               {blogData.map((item: BlogItemProps, index: number) => (

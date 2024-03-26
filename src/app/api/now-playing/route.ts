f135b6cf15edb4e354ff28d66a59582cd840d246
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-03-26 18:36:31 +0100

    feat(core): force api routes as dynamic routes

--- src/app/api/now-playing/route.ts
+++ src/app/api/now-playing/route.ts
@@ -19,3 +19,5 @@ export async function GET() {
     )
   }
 }
+
+export const dynamic = 'force-dynamic'
--- src/app/api/read-stats/route.ts
+++ src/app/api/read-stats/route.ts
@@ -25,3 +25,5 @@ export async function GET() {
     )
   }
 }
+
+export const dynamic = 'force-dynamic'

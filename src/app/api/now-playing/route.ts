24db7827517c4b3665b19e117284de8a9d321bd9
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-03-26 12:21:48 +0100

    feat(core): spotify works

---
+++ src/app/api/now-playing/route.ts
@@ -0,0 +1,21 @@
+import { getNowPlaying } from '@/services/spotify'
+
+export async function GET() {
+  try {
+    const spotifyData = await getNowPlaying()
+
+    return Response.json(spotifyData.data, {
+      headers: {
+        'Cache-Control': 'public, s-maxage=60, stale-while-revalidate=30',
+      },
+      status: 200,
+    })
+  } catch (error) {
+    return Response.json(
+      { message: 'Internal Server Error' },
+      {
+        status: 500,
+      },
+    )
+  }
+}

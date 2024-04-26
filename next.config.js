067eaf49ec9896ae57940cabb6dd24fce1ff1717
aulianza <aulianza.dev@gmail.com>
2024-04-26 09:32:51 +0700

    feat: allow all image url

--- next.config.js
+++ next.config.js
@@ -2,15 +2,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [
-      'aulianza.s3.ap-southeast-1.amazonaws.com',
-      'aulianza.id',
-      'cloud.aulianza.com',
-      'aulianza.com',
-      'dev-to-uploads.s3.amazonaws.com',
-      'res.cloudinary.com',
-      'lh3.googleusercontent.com',
-      'blog-api.aulianza.id',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
     ],
   },
 };
